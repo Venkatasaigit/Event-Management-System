9c844c218e0a0725a53ab81a61e517446c23d7a2
Sai Sandeep <venkatasai495@gmail.com>
2026-07-25 23:18:32 +0530

    https://event-management-system-3-82i4.onrender.com

--- login.js
+++ login.js
@@ -1,30 +1,22 @@
 function login() {
-
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
     const API = "https://event-management-system-2-ha7n.onrender.com";
-    fetch("http://localhost:3000/users")
+
+    fetch(`${API}/users`)
         .then(response => response.json())
         .then(users => {
-
             const user = users.find(
                 u => u.email === email && u.password === password
             );
 
             if (user) {
-
                 localStorage.setItem("user", JSON.stringify(user));
-
                 alert("Login Successful");
-
                 window.location.href = "index.html";
-
             } else {
-
                 alert("Invalid Email or Password");
-
             }
-
         })
         .catch(error => {
             console.error(error);
